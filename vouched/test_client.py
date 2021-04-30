bef8bd2444a6a00f297abc6304458c11bd1aa4a5
Marcus Oliver <marcus.oliver@vouched.id>
2021-04-29 21:19:04 -0700

    version 0.0.3

--- vouched/test_client.py
+++ vouched/test_client.py
@@ -1,7 +1,11 @@
 from vouched import config
 import requests
+import json
 from vouched.client import request_api
 import base64
+import os
+import hmac
+import hashlib
 
 def base64_image(file):
   return base64.encodestring(open(file,"rb").read())
@@ -60,4 +64,17 @@ def test_submit_invite():
   })
   assert len( r.get('errors') ) > 0
 
+def test_valid_webhook_signature():
+  token = os.getenv('API_KEY', 'vcP*ywx#zQ.K57#G3wfP!ZwR5xzqv~')
+  #when testing another request
+  #replace X_SIGNATURE with the value of the x-signature	header from the request to be tested
+  #replace the contents of response.txt with the stringified response from the webhookcall 
+  signature = os.getenv('X_SIGNATURE', 'BeXsClk0e/aIQfhPtHDKQzGnCj8=')
+  encoded_token =token.encode("utf-8")
+  with open('./data/response.txt') as text_file:
+    data = text_file.read()
+    data_encoded = data.encode("utf-8")
+    hmac_result = hmac.new(encoded_token, data_encoded, hashlib.sha1)
+    decodedByte = base64.b64encode(hmac_result.digest())
+    assert decodedByte.decode('utf-8') == signature
     
